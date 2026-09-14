d7c366c1c319479cb4507cd174c8cb47b534a1b2
BogaturevDesignStudio <Bogaturevjeka@i.ua>
2026-09-14 16:21:16 +0300

    Update projects.js

--- portfolio/projects.js
+++ portfolio/projects.js
@@ -22,7 +22,7 @@ const projects = [
     type: "Logo Design",
     role: "Graphic Designer",
     url: "tent-company-logo.html",
-    hero: "images/tent-company-logo/Logo Tent company.jpg"
+    hero: "images/tent-company-logo/tent-logo-design.jpg"
 },
     {
         id: "bbq",
