411132c72ea3eb692d717476ce3c9cbd9798a141
BogaturevDesignStudio <Bogaturevjeka@i.ua>
2026-09-23 15:19:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -620,15 +620,6 @@ toggleAboutBtn.addEventListener("click", () => {
   // Инициализация страницы с нужным языком
   setLanguage(lang);
 });
-toggleSkillsBtn.addEventListener("click", () => {
-  isSkillsVisible = !isSkillsVisible;
-  console.log('Skills visible:', isSkillsVisible);
-  skillsSection.classList.toggle("show", isSkillsVisible);
-  toggleSkillsBtn.textContent = isSkillsVisible ? "Скрыть навыки" : "Мои навыки";
-  if (isSkillsVisible) {
-    skillsSection.scrollIntoView({ behavior: "smooth" });
-  }
-});
 //фоновая анимация//
   
   const blobs = document.querySelectorAll('.blob'); 
